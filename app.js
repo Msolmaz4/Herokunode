14606c3e964394ff49dd28cd819fd2fce45f8d71
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-10-22 23:59:34 +0200

    er

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ import express from 'express'
 const app =express()
 const port = 4500
 
+
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.send('hayat guyeeeel')
+})
+
 app.listen(port,()=>{
     console.log('baglandik')
 })
