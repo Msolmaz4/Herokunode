07ff8f54acccabeb1de34d0573edbf89348e568a
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-10-23 09:17:44 +0200

    ejs

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ import express from 'express'
 const app =express()
 const port = 4500
 
+//ejs 
+app.set('view engine','ejs')
 
+//statick 
 app.use(express.static('public'))
 
 app.get('/',(req,res)=>{
-    res.send('hayat guyeeeel')
+    res.render('index')
+})
+app.get('/about',(req,res)=>{
+    res.render('about')
 })
 
 app.listen(port,()=>{
