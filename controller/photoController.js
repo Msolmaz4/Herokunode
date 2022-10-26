949f7dd9d13f66b1499d264fc8dd0b7e2fb0a3c1
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-10-26 16:58:25 +0200

    control

--- controller/photoController.js
+++ controller/photoController.js
@@ -4,10 +4,8 @@ import Photo from "../models/photoModels.js";
 const createPhoto = async (req, res) => {
   try {
     const photo = await Photo.create(req.body);
-    res.status(201).json({
-      succed: true,
-      photo,
-    });
+    res.status(201).redirect('/users/dashboard')
+    
   } catch (error) {
     res.status(500).json({
       succed: false,
